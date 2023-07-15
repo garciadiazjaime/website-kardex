08b679d28669dcfc1adafb9276e0521c9daac15e
jaime <garciadiazjaime@gmail.com>
2023-07-15 15:26:18 -0500

    feat: show content

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,41 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [content, setContent] = useState([]);
+  const [content, setContent] = useState<string[]>([]);
 
   const fileHandler = (event: any) => {
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.onload = function (event) {
+      if (!event.target) {
+        return;
+      }
+
       readPDF(event.target.result);
     };
 
     reader.readAsArrayBuffer(file);
   };
 
-  const readPDF = (buffer: any) => {
+  const readPDF = async (buffer: any) => {
+    // @ts-ignore
     const loadingTask = pdfjsLib.getDocument(buffer);
-    loadingTask.promise.then(function (pdf) {
-      pdf.getPage(1).then(async function (page) {
-        const textContent = await page.getTextContent();
-        const lines = textContent.items.map((item) => item.str);
-        console.log(lines)
-        setContent(lines);
-      });
+    loadingTask.promise.then((pdf: any) => {
+      for (let i = 1; i <= pdf.numPages; i += 1) {
+        pdf.getPage(i).then(async (page: any) => {
+          const textContent = await page.getTextContent();
+          const lines = textContent.items.map((item: any) => item.str);
+          setContent([...content, ...lines]);
+        });
+      }
     });
   };
+
+  useEffect(() => {
+    console.log(content)
+  }, [content])
 
   return (
     <main>

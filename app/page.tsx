4a90ccd4ee0187351756f2f85ad26d7c04abfe31
jaime <garciadiazjaime@gmail.com>
2023-08-12 21:04:27 -0500

    feat: work ui

--- app/page.tsx
+++ app/page.tsx
@@ -45,40 +45,88 @@ export default function Home() {
     for (let i = 0; i < content.length; i += 1) {
       const line = content[i];
       if (courseRegex.test(line)) {
-        const course = line
+        const course = line;
 
-        for (let j = i+1; j < content.length; j += 1) {
+        for (let j = i + 1; j < content.length; j += 1) {
           const line = content[j];
 
           if (dateRegex.test(line)) {
             _grades[course] = content[j - 2];
-            break
+            break;
           }
         }
-        
       }
     }
-    setGrades({...grades, ..._grades });
-  }
+    setGrades({ ...grades, ..._grades });
+  };
 
   useEffect(() => {
-    initGrades()
+    initGrades();
   }, [content]);
+
+  const styles = {
+    colors: {
+      primary: "#006733",
+      secondary: "#f1a631",
+      gray: "#565451",
+      gray800: "#EEE",
+    },
+    container: {
+      maxWidth: 1080,
+      margin: "0 auto",
+    },
+  };
 
   const getColor = (course) => {
     if (grades[course.code]) {
-      return grades[course.code] >= 60 ? "green" : "red";
+      return grades[course.code] >= 60 ? styles.colors.primary : "red";
     }
 
-    return course.optional ? "#EEE" : "";
+    return course.optional ? styles.colors.gray800 : "";
   };
 
   return (
     <main>
-      <div style={{ maxWidth: 1080, margin: "100px auto" }}>
-        <input type="file" onChange={fileHandler} />
+      <header
+        style={{
+          borderBottom: `4px solid ${styles.colors.secondary}`,
+          padding: "20px 0",
+          margin: "0 0 40px 0",
+        }}
+      >
+        <div style={styles.container}>
+          <h1 style={{ color: styles.colors.primary, fontWeight: "bold" }}>
+            UABC | Ingeniero en Electrónica | Plan 2020-1
+          </h1>
+        </div>
+      </header>
 
-        <div style={{ marginTop: 200, display: "flex" }}>
+      <div style={{ ...styles.container, padding: "0 0 60px 0" }}>
+        <div
+          style={{ padding: 6, cursor: "pointer" }}
+        >
+          <label
+            htmlFor="files"
+            style={{ display: "inline-block", paddingRight: 20 }}
+          >
+            Selecciona tu Kardex:{" "}
+          </label>
+          <input id="files" type="file" onChange={fileHandler} />
+        </div>
+      </div>
+
+      <div
+        style={{
+          maxWidth: 1080,
+          margin: "0 auto",
+          overflow: "scroll",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
           {Electronica.map((semester, index) => (
             <div key={`semester_${semester.name}`}>
               {semester.courses.map(
@@ -90,19 +138,27 @@ export default function Home() {
                   <div
                     key={course.code}
                     style={{
-                      width: 150,
-                      height: 150,
-                      margin: 12,
+                      width: (1080 - 12 * 8) / 8,
+                      height: (1080 - 12 * 8) / 8,
+                      margin: 6,
                       border: "1px solid black",
+                      backgroundColor: getColor(course),
                       display: "flex",
                       alignItems: "center",
-                      justifyContent: "center",
                       textAlign: "center",
-                      backgroundColor: getColor(course),
+                      justifyContent: "center",
                     }}
                   >
                     <div>
-                      <div>{course.name}</div>
+                      <div
+                        style={{
+                          wordBreak: "break-word",
+                          fontSize: ".9em",
+                          padding: 2,
+                        }}
+                      >
+                        {course.name}
+                      </div>
 
                       <div>{course.code}</div>
                     </div>
